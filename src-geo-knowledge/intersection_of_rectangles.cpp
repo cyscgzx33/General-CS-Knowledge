b1fc9b3f0855bc86d02fa3399ce8359f24dc3138
Yusen Chen <chenyus@umich.edu>
2020-07-18 00:35:18 -0400

    feat: added geometry related knowledge

---
+++ src-geo-knowledge/intersection_of_rectangles.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Point {
+public:
+    int x;
+    int y;
+    Point(int x, int y) : x(x), y(y) {}
+};
+
+
+int intersectionOfRectangles()
